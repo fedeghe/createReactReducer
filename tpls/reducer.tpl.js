097eed05a1d18872389a715842c94995655b9a79
fedeghe <fedeghe@gmail.com>
2024-05-27 09:10:51 +0200

    reducer tpl up

--- tpls/reducer.tpl.js
+++ tpls/reducer.tpl.js
@@ -6,10 +6,13 @@ const actions = {
         [ACTIONS.INIT]: ({ payload }) => init(payload),
         //ACTIONS
     },
-    reducer = (oldState, {payload, type}) => 
-        (type in actions)
+    reducer = (oldState, action) => {
+        const {payload, type} = action
+        if (typeof type === 'undefined') throw new Error(`Action '${action}' unexpected`)
+        return type in actions
             ? Object.assign({}, oldState, actions[type]({oldState, payload}))
             : oldState;
+    }
 export default {
     func: reducer,
     init: o => reducer({}, {type: ACTIONS.INIT, payload: o}),
